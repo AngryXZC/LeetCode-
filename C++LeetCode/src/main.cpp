ebd221a02a0d9bc60409740233fec0db6f181ade
AngryLeesin <xingzhicheng@imudges.com>
2023-10-16 16:57:01 +0800

    Java和C++实现
    代码
    
    测试用例
    测试用例
    测试结果
    测试结果
    通过
    通过
    通过
    
    260. 只出现一次的数字 III
    已解答
    中等
    相关标签
    相关企业
    给你一个整数数组 nums，其中恰好有两个元素只出现一次，其余所有元素均出现两次。 找出只出现一次的那两个元素。你可以按 任意顺序 返回答案。
    
    你必须设计并实现线性时间复杂度的算法且仅使用常量额外空间来解决此问题。

--- C++LeetCode/src/main.cpp
+++ C++LeetCode/src/main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <vector>
 
-int main(int argc, char *argv[])
+using namespace std;
+
+vector<int> singleNumber(vector<int> &nums)
 {
-	std::cout << "Hello world!" << std::endl;
+	int xor_result = 0;
+	for (int num : nums)
+	{
+		xor_result ^= num;
+	}
+
+	int mask = 1;
+	while ((xor_result & mask) == 0)
+	{
+		mask <<= 1;
+	}
+
+	int a = 0, b = 0;
+	for (int num : nums)
+	{
+		if ((num & mask) != 0)
+		{
+			a ^= num;
+		}
+		else
+		{
+			b ^= num;
+		}
+	}
+
+	return {a, b};
+}
+
+int main()
+{
+	vector<int> nums = {1, 2, 3, 2, 1, 4, 5};
+	vector<int> res = singleNumber(nums);
+
+	cout << res[0] << " " << res[1] << endl;
+
+	return 0;
 }
