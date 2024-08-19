e46418cafdfad8aea42478f07f780e6a9085983d
xzc <xingzhicheng@imudges.com>
2024-08-19 20:00:20 +0800

    快排解决第K个大小的元素

--- JavaLeetCode/app/src/main/java/javaleetcode/sort/SortSolution.java
+++ JavaLeetCode/app/src/main/java/javaleetcode/sort/SortSolution.java
@@ -5,8 +5,44 @@ import javaleetcode.huawei.ListNode;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Random;
 
 public class SortSolution {
+    /**
+     * 75. 颜色分类
+     * 给定一个包含红色、白色和蓝色、共 n 个元素的数组 nums ，原地 对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+     *
+     * 我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+     *
+     * 必须在不使用库内置的 sort 函数的情况下解决这个问题。
+     * */
+
+    public void sortColors(int[] nums) {
+
+        // nums[0...zero] == 0, nums[zero + 1, i] == 1, nums[two, n - 1] == 2
+        int zero = -1, i = 0, two = nums.length;
+        while(i < two){
+
+            if(nums[i] == 0){
+                zero ++;
+                swap(nums, zero, i);
+                i ++;
+            }
+            else if (nums[i] == 2){
+                two --;
+                swap(nums, i, two);
+            }
+            else{ // nums[i] == 0
+                i ++;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j){
+        int t = nums[i];
+        nums[i]= nums[j];
+        nums[j] = t;
+    }
     /**
      * 283. 移动零
      * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
@@ -31,6 +67,7 @@ public class SortSolution {
     /**
      * 215. 数组中的第 K 个最大元素
      */
+    //堆排序
     public int findKthLargest(int[] nums, int k) {
         int maxIndex = 0;
         for (int i = 0; i < k; i++) {
@@ -46,6 +83,53 @@ public class SortSolution {
         }
         return nums[k - 1];
     }
+
+    //快速排序
+    public int findKthLargest2(int[] nums, int k) {
+        Random rnd = new Random();
+        return selectK(nums, 0, nums.length - 1, nums.length - k, rnd);
+    }
+
+    private int selectK(int[] arr, int l, int r, int k, Random rnd){
+
+        int p = partition(arr, l, r, rnd);
+
+        if(k == p) return arr[p];
+
+        if(k < p) return selectK(arr, l, p - 1, k, rnd);
+        return selectK(arr, p + 1, r, k, rnd);
+    }
+
+    private int partition(int[] arr, int l, int r, Random rnd){
+
+        // 生成 [l, r] 之间的随机索引
+        int p = l + rnd.nextInt(r - l + 1);
+        swap(arr, l, p);
+
+        // arr[l+1...i-1] <= v; arr[j+1...r] >= v
+        int i = l + 1, j = r;
+        while(true){
+
+            while(i <= j && arr[i] < arr[l])
+                i ++;
+
+            while(j >= i && arr[j] > arr[l])
+                j --;
+
+            if(i >= j) break;
+
+            swap(arr, i, j);
+
+            i ++;
+            j --;
+        }
+
+        swap(arr, l, j);
+        return j;
+    }
+
+
+
 
     /**
      * 147. 对链表进行插入排序
